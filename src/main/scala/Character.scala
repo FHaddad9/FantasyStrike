053f1a593dcb0e1d1384064dfb5b92308d2c5589
fhaddad9 <ferashaddadgit@gmail.com>
2023-10-09 21:04:26 +0100

    Random attack hits based on strength, display health and check death

--- src/main/scala/Character.scala
+++ src/main/scala/Character.scala
@@ -1,9 +1,18 @@
-class Character(val name: String, var health: Int, val strength: Int, val defense: Int, val inventory: ArrayBuffer[String]){
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
+class Character(val name: String, var health: Int, var strength: Int, val defense: Int, val inventory: ArrayBuffer[String]){
   def attack(character: Character): Unit =
-    character.takeDamage(strength)
+    character.takeDamage(strength - Random.nextInt(strength+1))
 
   def takeDamage(attackAmount: Int): Unit =
     health -= attackAmount
+
+  def isDead: Boolean =
+    health <= 0
+
+  def showHealth: Unit =
+    println(s"$name's health: ${health}")
 
   def addItemToInventory(item: String): Unit =
     inventory += item
